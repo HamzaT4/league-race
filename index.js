29b2109e2d98a8de6755139a9048ef2526fc5f32
HamzaT4 <hamzatay2012@gmail.com>
2023-08-02 02:51:05 +0300

    Make responsive design and ingame feature

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const hamWR=document.querySelector(".h-wr-txt");
 const karRank=document.querySelector(".k-rank-txt");
 const karLP=document.querySelector(".k-lp-txt");
 const karWR=document.querySelector(".k-wr-txt");
-const timerDiv= document.querySelector(".timer")
+const timerDiv= document.querySelector(".timer");
+const liveIconHam = document.querySelector(".live-ham");
+const liveIconKar =document.querySelector(".live-kar")
 
 
 let  countDownDate = new Date("Aug 31, 2023 23:59:59").getTime();
@@ -22,18 +24,47 @@ var x = setInterval(function() {
     + minutes + "m " + seconds + "s ";
 });
 
+function showIngame(user) {
+    if (user == "hamza") {
+        liveIconHam.style.visibility="visible";
+        console.log("hamza 3m bl3b");
+    }
+    else if(user == "kareem"){
+        liveIconKar.style.visibility="visible";
+        console.log("kareem 3m bl3b");
+    }
+}
 
 async function getId() {
 
- const response3 = await fetch("https://eun1.api.riotgames.com/lol/league/v4/entries/by-summoner/HCn8bAfvK2O4b1gAKCXzHvHa_n0BkveUJme2TNMkTebtWzkbUjkyCSaz5A?api_key=RGAPI-7a868080-7d49-4578-8b17-c06d6cbdfc66", {
+ const response3 = await fetch("https://eun1.api.riotgames.com/lol/league/v4/entries/by-summoner/-c9IwmnMpEYKhW8CeHyr64UFqxTqn7EJMrDQa_jZUHskAJbRXGHeZx1Dbg?api_key=RGAPI-a5ac701e-9222-4068-b476-e4a1d591acbc", {
     mode: 'cors'
   });
  const data3 = await response3.json();
- const response4 =await fetch("https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/DCDMwLJe8g-1AOmM0znpdZC6gRlMhBw3zKEdL-xkPGTHOPtF9b6AXutl_w?api_key=RGAPI-7a868080-7d49-4578-8b17-c06d6cbdfc66", {
+ const response4 =await fetch("https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/KyUYNiF_B6oa0CGsz-ACG24atCQuawNX2XvFmoED8gZyLsQ3HD2sF2HfWQ?api_key=RGAPI-a5ac701e-9222-4068-b476-e4a1d591acbc", {
     mode: 'cors'
   });
- const data4 = await response4.json()
- const hamzaData = data3[1]
+  try {
+    const ingameResHam= await fetch("https://eun1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/p0c9VEA1ly79ybyAPiyHipbqLj4xSHYSAa2Xgo5YVU-_k6nc?api_key=RGAPI-a5ac701e-9222-4068-b476-e4a1d591acbc", {
+    mode: 'cors'
+  });
+    showIngame("hamza")
+  } catch (error) {
+    console.log(error);
+  }
+  try {
+    const ingameResKar = await fetch("https://na1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/KyUYNiF_B6oa0CGsz-ACG24atCQuawNX2XvFmoED8gZyLsQ3HD2sF2HfWQ?api_key=RGAPI-a5ac701e-9222-4068-b476-e4a1d591acbc", {
+        mode: 'cors'
+      }) ;
+      showIngame("kareem")
+
+  } catch (error) {
+    console.log(error);
+  }
+
+
+const data4 = await response4.json();
+const hamzaData = data3[1]
 const kareemData = data4[0] 
 const hamTier = document.createElement("img");
 const karTier = document.createElement("img");
